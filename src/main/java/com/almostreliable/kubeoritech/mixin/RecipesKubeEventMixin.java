9c2179079a3149c571df9ada60c265dcb5f8f384
rlnt <relentless@rlnt.dev>
2026-02-08 12:15:01 +0100

    fix non Oritech recipes being completely removed
    fixes #1

--- src/main/java/com/almostreliable/kubeoritech/mixin/RecipesKubeEventMixin.java
+++ src/main/java/com/almostreliable/kubeoritech/mixin/RecipesKubeEventMixin.java
@@ -16,6 +16,6 @@ public abstract class RecipesKubeEventMixin {
 
     @ModifyReceiver(method = "applyChanges", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;peek(Ljava/util/function/Consumer;)Ljava/util/stream/Stream;", ordinal = 1), remap = false)
     private Stream<KubeRecipe> kubejs_oritech$filterInvalidRecipes(Stream<KubeRecipe> instance, Consumer<? super KubeRecipe> peekConsumer) {
-        return instance.filter(t -> t instanceof OritechKubeRecipe recipe && recipe.valid);
+        return instance.filter(t -> !(t instanceof OritechKubeRecipe recipe) || recipe.valid);
     }
 }
